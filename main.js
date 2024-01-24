453f171615c30938394b9feefb5a228b22318305
islamayman <islamwebdevelopment@gmail.com>
2024-01-24 13:36:55 +0200

    first commit

---
+++ main.js
@@ -0,0 +1,25 @@
+var Text1=document.getElementById('text1');
+var Text2=document.getElementById('text2');
+
+
+
+
+
+
+
+/*-----------------Make Button Read More and More Less------*/
+function myFunction() {
+    var dots = document.getElementById("dots");
+    var moreText = document.getElementById("more");
+    var btnText = document.getElementById("myBtn");
+  
+    if (dots.style.display === "none") {
+      dots.style.display = "inline";
+      btnText.innerHTML = "Read more";
+      moreText.style.display = "none";
+    } else {
+      dots.style.display = "none";
+      btnText.innerHTML = "Read less";
+      moreText.style.display = "inline";
+    }
+  }
